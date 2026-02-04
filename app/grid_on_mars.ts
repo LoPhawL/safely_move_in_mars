8941aa4bf208fad751a9421b0ff72e179bd13a21
Surendra Varshu <r.harip3@gmail.com>
2026-02-04 17:01:56 +0000

    progress, completion: first result matching the expected output

--- app/grid_on_mars.ts
+++ app/grid_on_mars.ts
@@ -3,7 +3,6 @@ import { TRobotOrientation } from "./types/robotOrientation.type";
 import { TRobotRotation } from "./types/robotRotation.type";
 import { rotateRobot } from "./utils/rotateRobot";
 
-
 function analyseRobotPositions (
     gridConfiguration: [number, number],
     robotsSettings: { 
@@ -22,7 +21,10 @@ function analyseRobotPositions (
     const bound_x_1 = gridConfiguration[0];
     const bound_y_1 = gridConfiguration[1];
 
-    for (const robotSetting of robotsSettings) {
+    const losingPositions: string[] //[number, number, TRobotOrientation][]
+     = [];
+
+    eachRobot: for (const robotSetting of robotsSettings) {
 
         // initialize robot
         let robotCoordinate: TRobotCoordinate = JSON.parse(JSON.stringify(robotSetting.initialPosition.coordinate));
@@ -31,38 +33,56 @@ function analyseRobotPositions (
         // for type safety
         const arrayOfMovementInstructions: TRobotRotation[] = robotSetting.movementInstructions.split('') as TRobotRotation[];
 
-        for (let instruction of arrayOfMovementInstructions) {
+        instructionOfRobot: for (let instruction of arrayOfMovementInstructions) {
 
             //rotate
             if (instruction === 'L' || instruction === 'R') {
 
                 robotOrientation = rotateRobot(robotOrientation, instruction);
-                continue;
-            }
+                
+            } else if (instruction === 'F') {
 
-            if (instruction === 'F') {
+                if (losingPositions.includes(`${robotCoordinate[0]} ${robotCoordinate[1]} ${robotOrientation}`)) {
+                    
+                    continue instructionOfRobot;
+                } 
 
                 let newRobotCoordinate: TRobotCoordinate;
                 // move forward
                 if (robotOrientation === 'N') {
+
                     // x = x, y = y + 1
                     newRobotCoordinate = [robotCoordinate[0], robotCoordinate[1] + 1];
-
                 } else if (robotOrientation === 'E') { 
+
                     // x = x + 1, y = y
                     newRobotCoordinate = [robotCoordinate[0] + 1, robotCoordinate[1]];
-
                 } else if (robotOrientation === 'S') {
+
                     // x = x, y = y - 1
                     newRobotCoordinate = [robotCoordinate[0], robotCoordinate[1] - 1];
+                } else {
 
-                } else
                     // x = x - 1, y = y
                     newRobotCoordinate = [robotCoordinate[0] - 1, robotCoordinate[1]];
                 }
-            }
 
+                if (newRobotCoordinate[0] < bound_x_0 || newRobotCoordinate[0] > bound_x_1 || newRobotCoordinate[1] < bound_y_0 || newRobotCoordinate[1] > bound_y_1) {
+
+                    // the robot would have been lost
+                    losingPositions.push(`${robotCoordinate[0]} ${robotCoordinate[1]} ${robotOrientation}`);
+                    console.log(`${robotCoordinate[0]} ${robotCoordinate[1]} ${robotOrientation} LOST`);
+                    continue eachRobot;
+                } else {
+
+                    // move it forward
+                    robotCoordinate = newRobotCoordinate;
+                }
+            }
         }
+        console.log(`${robotCoordinate[0]} ${robotCoordinate[1]} ${robotOrientation}`);
+
+    }
 }
 
 
